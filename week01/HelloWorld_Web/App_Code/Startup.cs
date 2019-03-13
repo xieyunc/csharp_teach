a1589cac1faf0d2f511dad754845196a2fed9cc3
xieyunc-dmxy <xieyunc@qq.com>
2019-03-13 13:59:19 +0800

    加入版本控制

---
+++ week01/HelloWorld_Web/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloWorld_Web.Startup))]
+namespace HelloWorld_Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
