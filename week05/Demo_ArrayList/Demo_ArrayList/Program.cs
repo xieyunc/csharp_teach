01069d18811b0c78a3c66cfbe680fba4ef633a48
xieyunc-dmxy <xieyunc@qq.com>
2019-03-27 15:42:21 +0800

    增加了week05的内容

---
+++ week05/Demo_ArrayList/Demo_ArrayList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Demo_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {2,5,1,19,81,23,4,6};
+
+            ArrayList List = new ArrayList(a); 
+
+            Console.WriteLine("数组的原始值：");
+            for(int i=0;i<a.Length;i++)
+            {
+                Console.Write(a[i]+" ");
+            }
+            Console.WriteLine();
+
+            List.Add(66);
+            List.Insert(3, 44);
+            List.Remove(5);
+            List.RemoveAt(3);
+
+            List.RemoveRange(5, 2);
+
+            Console.WriteLine("List的原始值：");
+            foreach (int x in List)
+            {
+                Console.Write(x+" ");
+            }
+
+            Console.WriteLine();
+            List.Sort();
+            Console.WriteLine("List排序之后的值：");
+            foreach (int x in List)
+            {
+                Console.Write(x + " ");
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ week05/Demo_HashTable/Demo_HashTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Demo_HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable(); 
+            hashtable.Add("id", "BH0001"); 
+            hashtable.Add("name", "TM");
+            //hashtable.Add("id", "BH0002");
+            foreach (DictionaryEntry dicEntry in hashtable)
+              Console.WriteLine("\t " + dicEntry.Key + "\t " + dicEntry.Value);
+
+            Console.WriteLine(hashtable.ContainsValue("id"));
+            System.Console.ReadKey();
+        }
+        
+    }
+}
