2b3709d068bf3cae7c7c6a1cec3a6d73b0372bdf
xieyunc-dmxy <xieyunc@qq.com>
2019-06-05 15:39:16 +0800

    增加了week14的内容

---
+++ week14/Demo_SqlEdit/Demo_SqlEdit/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_SqlEdit
+{
+    public partial class Form1 : Form
+    {
+        private void InitEditControl()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "男";
+            textBox4.Text = "2019";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitEditControl();
+            textBox1.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitEditControl();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string xh, xm, xb, nj, xy, zy, bj, dh, sError;//注释一下
+            sError = "";
+            MsSql mySql = new MsSql();
+            xh = textBox1.Text.Trim();
+            xm = textBox2.Text.Trim();
+            xb = textBox3.Text.Trim();
+            nj = textBox4.Text.Trim();
+            xy = textBox5.Text.Trim();
+            zy = textBox6.Text.Trim();
+            bj = textBox7.Text.Trim();
+            dh = textBox8.Text.Trim();
+
+            int iResult = mySql.InsertData(xh,xm,xb,nj,xy,zy,bj,dh,out sError);
+            if (iResult>0)
+                MessageBox.Show(string.Format("数据库插入成功！{0}条记录已更新！",iResult));
+            else
+                MessageBox.Show(string.Format("数据库插入失败！{0}", sError));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string xh = textBox9.Text.Trim();
+            string xm, xb, nj, xy, zy, bj, dh, sError;
+
+            MsSql mySql = new MsSql();
+            if (mySql.LocateXsInfo(xh, out xm, out xb, out nj, out xy, out zy, out bj, out dh,out sError))
+            {
+                textBox1.Text = xh;
+                textBox2.Text = xm;
+                textBox3.Text = xb;
+                textBox4.Text = nj;
+                textBox5.Text = xy;
+                textBox6.Text = zy;
+                textBox7.Text = bj;
+                textBox8.Text = dh;
+                textBox1.ReadOnly = true;
+                textBox1.Focus();
+            }
+            else
+            {
+                MessageBox.Show(string.Format("学号为{0}的学生不存在！"+sError,xh));
+                InitEditControl();
+                textBox1.ReadOnly = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            string xh, xm, xb, nj, xy, zy, bj, dh, sError;//注释一下
+            sError = "";
+            MsSql mySql = new MsSql();
+            xh = textBox1.Text.Trim();
+            xm = textBox2.Text.Trim();
+            xb = textBox3.Text.Trim();
+            nj = textBox4.Text.Trim();
+            xy = textBox5.Text.Trim();
+            zy = textBox6.Text.Trim();
+            bj = textBox7.Text.Trim();
+            dh = textBox8.Text.Trim();
+
+            int iResult = mySql.UpdateData(xh, xm, xb, nj, xy, zy, bj, dh, out sError);
+            if (iResult > 0)
+                MessageBox.Show(string.Format("数据库更新成功！{0}条记录已更新！", iResult));
+            else
+                MessageBox.Show(string.Format("数据库更新失败！{0}", sError));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            string xh,sError;//注释一下
+            sError = "";
+            MsSql mySql = new MsSql();
+            xh = textBox1.Text.Trim();
+
+            int iResult = mySql.DeleteData(xh, out sError);
+            if (iResult > 0)
+            {
+                MessageBox.Show(string.Format("数据库删除成功！{0}条记录已更新！", iResult));
+                InitEditControl();
+            }
+            else
+                MessageBox.Show(string.Format("数据库删除失败！{0}", sError));
+        }
+    }
+}
---
+++ week14/Demo_SqlEdit/Demo_SqlEdit/MsSql.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Demo_SqlEdit
+{
+    class MsSql
+    {
+        private SqlConnection sqlConn;
+
+        public MsSql()
+        {
+            sqlConn = new SqlConnection();
+            ConnDbSrv();
+        }
+
+        public int InsertData(string xh,string xm,string xb,string nj,string xy,string zy,string bj,string dh,out string sError)
+        {
+            string sqlStr;
+
+            sqlStr = string.Format("insert into Table_Xsxx(学号,姓名,性别,年级,学院,专业,班级,电话) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')",xh,xm,xb,nj,xy,zy,bj,dh);
+
+            SqlCommand cmd = sqlConn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = sqlStr;
+            try
+            {
+                int iResult = cmd.ExecuteNonQuery();
+                sError = "";
+                return iResult;
+            }
+            catch (Exception ex)
+            {
+                sError = ex.Message;
+                return 0;
+                //throw;
+            }
+        }
+
+        public int UpdateData(string xh, string xm, string xb, string nj, string xy, string zy, string bj, string dh, out string sError)
+        {
+            string sqlStr;
+
+            sqlStr = string.Format("update Table_Xsxx set 姓名='{1}',性别='{2}',年级='{3}',学院='{4}',专业='{5}',班级='{6}',电话='{7}' where 学号='{0}'", xh, xm, xb, nj, xy, zy, bj, dh);
+
+            SqlCommand cmd = sqlConn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = sqlStr;
+            try
+            {
+                int iResult = cmd.ExecuteNonQuery();
+                sError = "";
+                return iResult;
+            }
+            catch (Exception ex)
+            {
+                sError = ex.Message;
+                return 0;
+                //throw;
+            }
+        }
+
+        public int DeleteData(string xh, out string sError)
+        {
+            string sqlStr;
+
+            sqlStr = string.Format("delete from Table_Xsxx where 学号='{0}'", xh);
+
+            SqlCommand cmd = sqlConn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = sqlStr;
+            try
+            {
+                int iResult = cmd.ExecuteNonQuery();
+                sError = "";
+                return iResult;
+            }
+            catch (Exception ex)
+            {
+                sError = ex.Message;
+                return 0;
+                //throw;
+            }
+        }
+
+        public bool LocateXsInfo(string xh,out string xm,out string xb,out string nj,out string xy,out string zy,out string bj,out string dh,out string sError)
+        {
+            string sqlStr;
+            xm = "";
+            xb = "";
+            xy = "";
+            nj = "";
+            zy = "";
+            bj = "";
+            dh = "";
+            sError = "";
+
+            sqlStr = string.Format("select * from Table_Xsxx where 学号='{0}'", xh);
+
+            SqlCommand cmd = sqlConn.CreateCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = sqlStr;
+            try
+            {
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+                while (sdr.Read())
+                {
+                    //xh = sdr["学号"].ToString();
+                    xm = sdr["姓名"].ToString();
+                    xb = sdr["性别"].ToString();
+                    nj = sdr["年级"].ToString();
+                    xy = sdr["学院"].ToString();
+                    zy = sdr["专业"].ToString();
+                    bj = sdr["班级"].ToString();
+                    dh = sdr["电话"].ToString();
+                }
+                return (xm != "");
+            }
+            catch (Exception ex)
+            {
+                sError = ex.Message;
+                return false;
+                //throw;
+            }
+        }
+
+        public bool ConnDbSrv()
+        {
+            sqlConn.Close();
+            sqlConn.ConnectionString = "server=(local)\\sqlexpress;database=xscj_db;uid=xscjgl_sa;pwd=abcd@1234;";
+            try 
+	        {	        
+                sqlConn.Open();
+                return sqlConn.State == System.Data.ConnectionState.Open;
+	        }
+	        catch (Exception)
+	        {
+                return false;		
+		        //throw;
+	        }
+        }
+        //~MsSql()
+        //{
+        //    if (sqlConn!=null)
+        //        sqlConn.Dispose();
+        //}
+    }
+}
